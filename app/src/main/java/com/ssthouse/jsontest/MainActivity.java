2cc0d344b6d7052d5b6f0c730673c86e1ec2edaf
ssthouse <ssthouse@163.com>
2016-10-31 20:47:48 +0800

    Toast the json object string parsed

--- app/src/main/java/com/ssthouse/jsontest/MainActivity.java
+++ app/src/main/java/com/ssthouse/jsontest/MainActivity.java
@@ -1,13 +1,67 @@
 package com.ssthouse.jsontest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String STR_JSON_OBJ = "{name:\"ssthouse\",age:21,school:\"HuaKe\"}";
+    private static final String STR_JSON_ARRAY = "{array:[{name:\"name1\",age:1,school:\"school1\"},{name:\"" +
+            "name2\",age:2,school:\"school2\"},{name:\"name3\",age:3,school:\"school3\"}]}";
+
+    @BindView(R.id.et_json_str)
+    EditText etJsonStr;
+    @BindView(R.id.id_btn_extract_json_obj)
+    Button idBtnExtractJsonObj;
+    @BindView(R.id.id_btn_extract_json_array)
+    Button idBtnExtractJsonArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
+    }
+
+    @OnClick(R.id.id_btn_extract_json_obj)
+    void parseJsonObj(){
+        etJsonStr.setText(STR_JSON_OBJ);
+        Person person = new Person();
+        try {
+            JSONObject jsonObject = new JSONObject(STR_JSON_OBJ);
+            person.name = jsonObject.getString("name");
+            person.age = jsonObject.getInt("age");
+            person.school = jsonObject.getString("school");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        toastPersonObj(person);
+    }
+
+    @OnClick(R.id.id_btn_extract_json_array)
+    void parseJsonArray(){
+
+    }
+
+    private void toastPersonObj(Person person) {
+        Toast.makeText(this, person.toString(), Toast.LENGTH_LONG).show();
+    }
+
+    private void tostPersonArray(List<Person> personList) {
+
     }
 }
---
+++ app/src/main/java/com/ssthouse/jsontest/Person.java
@@ -0,0 +1,17 @@
+package com.ssthouse.jsontest;
+
+/**
+ * Created by ssthouse on 31/10/2016.
+ */
+
+public class Person {
+
+    String name;
+    int age;
+    String school;
+
+    @Override
+    public String toString() {
+        return "name:" + name + "  age:" + age + " school:" + school;
+    }
+}
